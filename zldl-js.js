11643276ce225712852aed05085ba22345e8f6d7
zhaolandelong <zhaolandelong@163.com>
2016-04-07 10:08:19 +0800

    Update zldl-js.js

--- zldl-js.js
+++ zldl-js.js
@@ -1,24 +1,36 @@
 /**
  * Created by zhaoldl on 2016/3/17.工具类
  */
-!function(){
+! function() {
     var e = {
+        //判断是否是Android
+        isAndroid: navigator.userAgent.indexOf('Android') != -1 || navigator.userAgent.indexOf('Linux') != -1,
         //获取url中的参数
-        urlParam : function(param){
+        urlParam: function(param) {
             var reg = new RegExp('(^|&)' + param + '=([^&]*)(&|$)'),
                 r = window.location.search.substr(1).match(reg);
-            return r!=null?decodeURIComponent(r[2]):null;
+            return r != null ? decodeURIComponent(r[2]) : null;
         },
         //获取cookie
-        getCookie : function(name) {
-            var arr = [],reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
-            return (arr=document.cookie.match(reg))?arr[2]:null;
+        getCookie: function(name) {
+            var arr = [],
+                reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
+            return (arr = document.cookie.match(reg)) ? arr[2] : null;
         },
-        //专门获取JSON的ajax，url ajax url，type 请求方式类型'GET','POST'，data 要传的数据，succFun 成功回调，failFun 失败回调, async是否异步
-        ajaxForJson : function(url, type, data, succFun, failFun, async){
+        /**
+         * [ajaxForJson description]
+         * @param  {[type]} url     [ajax url]
+         * @param  {[type]} type    ['GET','POST',...]
+         * @param  {[type]} data    [jsondata]
+         * @param  {[type]} succFun [successed function]
+         * @param  {[type]} failFun [failed function]
+         * @param  {[type]} async  [true false]
+         * @return {[type]}         [description]
+         */
+        ajaxForJson: function(url, type, data, succFun, failFun, async) {
             //创建XMLHttpRequest对象
             var XMLHttpReq,
-            _async = typeof(async)=='undefined'||async;
+                _async = typeof(async) == 'undefined' || async;
             try {
                 XMLHttpReq = new ActiveXObject("Msxml2.XMLHTTP"); //IE高版本创建XMLHTTP
             } catch (E) {
@@ -44,34 +56,38 @@
             };
             XMLHttpReq.send(data);
         },
-        //判断是否是Android
-        isAndroid : navigator.userAgent.indexOf('Android') != -1 || navigator.userAgent.indexOf('Linux') != -1,
+        //两数相加，防止出现0.1+0.7==>0.79999999999
+        add: function(a, b) {
+            var r1, r2, m;
+            try {
+                r1 = a.toString().split('.')[1].length;
+            } catch (e) {
+                r1 = 0;
+            }
+            try {
+                r2 = b.toString().split('.')[1].length;
+            } catch (e) {
+                r2 = 0;
+            }
+            m = Math.pow(10, Math.max(r1, r2));
+            return (a * m + b * m) / m;
+        },
         //自动移动到可视区域，如果是IOS直接滚，如果是android，已唤出输入法就直接滚否则要监听onresize事件,注意IE不支持outerHeight
-        scrollIntoView : function(dom){
-            if((navigator.userAgent.indexOf('Android') == -1 && navigator.userAgent.indexOf('Linux') == -1)||(document.body.offsetHeight < window.outerHeight)){
+        scrollIntoView: function(dom) {
+            if ((navigator.userAgent.indexOf('Android') == -1 && navigator.userAgent.indexOf('Linux') == -1) || (document.body.offsetHeight < window.outerHeight)) {
                 dom.scrollIntoView(true);
-            }else{
-                window.onresize = function(){
+            } else {
+                window.onresize = function() {
                     dom.scrollIntoView(true);
                 }
             }
         },
-        //两数相加，防止出现0.1+0.7==>0.79999999999
-		mathAdd : function(a, b) {
-			var r1, r2, m;
-			try {
-				r1 = a.toString().split('.')[1].length;
-			} catch (e) {
-				r1 = 0;
-			}
-			try {
-				r2 = b.toString().split('.')[1].length;
-			} catch (e) {
-				r2 = 0;
-			}
-			m = Math.pow(10, Math.max(r1, r2));
-			return (a * m + b * m) / m;
-		},
+        //获取url全路径
+        path: function() {
+            var e = location.href,
+                i = e.lastIndexOf('/');
+            return e.substring(0, i + 1);
+        }
     };
     // 对Date的扩展，将 Date 转化为指定格式的String
     // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
@@ -96,7 +112,7 @@
                 fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
         return fmt;
     };
-    "object" == typeof exports ? module.exports = e : "function" == typeof define && (define.cmd || define.amd) ? define(function () {
+    "object" == typeof exports ? module.exports = e : "function" == typeof define && (define.cmd || define.amd) ? define(function() {
         return e
     }) : this.util = e;
 }()
