13e343f65f078d3267cf6b6f7beef5b75445822a
zhaolandelong <zhaolandelong@163.com>
2016-10-17 15:58:52 +0800

    sv

--- pagination/pagination-zldl-1.0.js
+++ pagination/pagination-zldl-1.0.js
@@ -1,1 +1,127 @@
-!function(){"use strict";var e=document.createElement("div");e.innerHTML="x<style>#pagination{-moz-user-select: none;-o-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;margin: 0 auto; padding: 5px; font-size: 14px; text-align: center;color: #666;} #pagination>.page,#pagination>.prev,#pagination>.next{display: inline-block; box-sizing: border-box; padding: 7px 13px; border: 1px solid #d1d1d1; background-color: #ffffff; cursor: pointer; margin: 0 3px} #pagination>.active{color: #fff; border: 1px solid #55626d; background: #55626d;}</style>",document.getElementsByTagName("head")[0].appendChild(e.lastChild);var n={num:1,maxnum:6,maxpage:1,current:1,dom:document.getElementById("pagination"),callback:function(){},pageTo:function(e){console.log("turn to page",e),n.callback(e),n.rend(e)},rend:function(e){var a=n.num,o=n.maxpage,t=Math.round(n.maxnum/2),i=n.dom,r=i.querySelector(".prev"),s=i.querySelector(".next"),l=i.querySelector(".ldot"),c=i.querySelector(".rdot"),p=i.querySelectorAll(".page");r.style.display=1==e?"none":"inline-block",s.style.display=e==o?"none":"inline-block";for(var d,u,m,g=0,f=a;g<f;g++)d=p[g],m=0==g?g+1:g==f-1?o:e<=t?g+1:e>=o-t+1?o-a+g+1:e-t+g+1,d.innerHTML=m,u=d.className,u.indexOf("active")!=-1?m!=e&&(d.className=u.replace(" active","")):m==e&&(d.className=u+" active");o>a&&(l.style.display=2==p[1].innerHTML?"none":"",c.style.display=p[a-2].innerHTML==o-1?"none":"")},init:function(e,a){console.log("pagination initialization success!");var o=n.dom;if(!o)return void console.log("如要添加分页，请在html中加入id为pagination的div");if("number"==typeof e){if(e<=1)return o.style.display="none",void console.log("less than 1");o.style.display="block",n.maxpage=e,console.log("more than 1")}else console.log("maxpage must be number!");a&&"function"==typeof a&&(n.callback=a);var t=n.maxpage,i=n.num=n.maxnum>t?t:n.maxnum;n.current=1;for(var r,s=['<span style="display:none;" class="prev">上一页</span><span class="page active">1</span><span class="ldot" style="display:none"> … </span>'],l=1;l<i;l++){if(r=l+1,l==i-1){var c=' style="display:none"';t>i&&(r=t,c=""),s.push('<span class="rdot"'+c+"> … </span>")}s.push('<span class="page">'+r+"</span>")}s.push('<span class="next">下一页</span>'),o.innerHTML=s.join("")}};n.dom.onclick=function(e){var e=e||window.event,a=e.target||e.srcElement;switch(a.className){case"page":if(n.current!=+a.innerHTML){var o=n.current=+a.innerHTML;n.pageTo(o)}break;case"prev":if(n.current>1){var o=--n.current;n.pageTo(o)}break;case"next":if(n.current<n.maxpage){var o=++n.current;n.pageTo(o)}}},"object"==typeof exports?module.exports=n:"function"==typeof define&&(define.cmd||define.amd)?define(function(){return n}):window.pagination=n}();
+//纯原生无依赖
+! function() {
+    "use strict";
+    //为了兼容ie8，别嫌奇怪
+    var domTmp = document.createElement('div');
+    domTmp.innerHTML = 'x<style>#pagination{-moz-user-select: none;-o-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;margin: 0 auto; padding: 5px; font-size: 14px; text-align: center;color: #666;} #pagination>.page,#pagination>.prev,#pagination>.next{display: inline-block; box-sizing: border-box; padding: 7px 13px; border: 1px solid #d1d1d1; background-color: #ffffff; cursor: pointer; margin: 0 3px} #pagination>.active{color: #fff; border: 1px solid #55626d; background: #55626d;}</style>'
+    document.getElementsByTagName('head')[0].appendChild(domTmp.lastChild);
+    var p = {
+        num: 1, //当前item数
+        maxnum: 6, //最大item数
+        maxpage: 1, //最大页码
+        current: 1, //当前页码
+        dom: document.getElementById('pagination'), //父dom
+        callback: function() {},
+        pageTo: function(n) {
+            console.log('turn to page', n);
+            p.callback(n);
+            p.rend(n);
+        },
+        rend: function(n) {
+            var _num = p.num,
+                _max = p.maxpage,
+                mid = Math.round(p.maxnum / 2),
+                start = 2,
+                dom = p.dom, //父dom
+                domPrev = dom.querySelector('.prev'), //上一页
+                domNext = dom.querySelector('.next'), //下一页
+                domLdot = dom.querySelector('.ldot'), //左省略号
+                domRdot = dom.querySelector('.rdot'), //右省略号
+                domPage = dom.querySelectorAll('.page'); //页码
+            domPrev.style.display = n == 1 ? 'none' : 'inline-block';
+            domNext.style.display = n == _max ? 'none' : 'inline-block';
+            for (var i = 0, l = _num, tmp, tmpClass, txt; i < l; i++) {
+                tmp = domPage[i];
+                if (i == 0) {
+                    txt = i + 1;
+                } else if (i == l - 1) {
+                    txt = _max;
+                } else {
+                    if (n <= mid) { //比较小时
+                        txt = i + 1;
+                    } else if (n >= _max - mid + 1) { //比较大时
+                        txt = _max - _num + i + 1;
+                    } else { //适中时
+                        txt = n - mid + i + 1;
+                    }
+                }
+                tmp.innerHTML = txt;
+                tmpClass = tmp.className;
+                tmpClass.indexOf('active') != -1 ? (txt != n && (tmp.className = tmpClass.replace(' active', ''))) : (txt == n && (tmp.className = tmpClass + ' active'));
+            }
+            if (_max > _num) { //省略号
+                domLdot.style.display = domPage[1].innerHTML == 2 ? 'none' : '';
+                domRdot.style.display = domPage[_num - 2].innerHTML == _max - 1 ? 'none' : '';
+            }
+        },
+        init: function(maxpage, callback) {
+            console.log('pagination initialization success!');
+            var _pagination = p.dom;
+            if (!_pagination) {
+                console.log('如要添加分页，请在html中加入id为pagination的div');
+                return;
+            }
+            if (typeof(maxpage) == 'number') {
+                if (maxpage <= 1) {
+                    _pagination.style.display = 'none';
+                    console.log('less than 1');
+                    return;
+                } else {
+                    _pagination.style.display = 'block';
+                    p.maxpage = maxpage;
+                    console.log('more than 1');
+                }
+            } else {
+                console.log('maxpage must be number!');
+            }
+            if (callback && typeof(callback) == 'function') {
+                p.callback = callback;
+            }
+            var _max = p.maxpage,
+                _num = p.num = p.maxnum > _max ? _max : p.maxnum;
+            p.current = 1;
+            var domArr = ['<span style="display:none;" class="prev">上一页</span><span class="page active">1</span><span class="ldot" style="display:none"> … </span>'];
+            for (var i = 1, tempTxt; i < _num; i++) {
+                tempTxt = i + 1;
+                if (i == _num - 1) {
+                    var show = ' style="display:none"';
+                    if (_max > _num) {
+                        tempTxt = _max;
+                        show = '';
+                    }
+                    domArr.push('<span class="rdot"' + show + '> … </span>');
+                }
+                domArr.push('<span class="page">' + tempTxt + '</span>');
+            }
+            domArr.push('<span class="next">下一页</span>');
+            _pagination.innerHTML = domArr.join('');
+        }
+    };
+    p.dom.onclick = function(e) {
+        var e = e || window.event,
+            self = e.target || e.srcElement; //兼容ie8
+        switch (self.className) {
+            case 'page':
+                if (p.current != +self.innerHTML) {
+                    var n = p.current = +self.innerHTML;
+                    p.pageTo(n);
+                }
+                break;
+            case 'prev':
+                if (p.current > 1) {
+                    var n = --p.current;
+                    p.pageTo(n);
+                }
+                break;
+            case 'next':
+                if (p.current < p.maxpage) {
+                    var n = ++p.current;
+                    p.pageTo(n);
+                }
+                break;
+        }
+    };
+    "object" == typeof exports ? module.exports = p : "function" == typeof define && (define.cmd || define.amd) ? define(function() {
+        return p
+    }) : window.pagination = p;
+}()
