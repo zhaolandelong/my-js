ce45487e69ea96bedd4edfd1efccaf78c095db1f
zhaolandelong <zhaolandelong@58ganji.com>
2017-06-21 13:50:45 +0800

    sv

--- ui/test.js
+++ ui/test.js
@@ -1,155 +1,267 @@
 ! function() {
     "use strict ";
     var PLUGINNAME = "uiZldl";
-    var NEWNAME = "uiZldl";
-    var STYLE = '';
-    var styleObj = {
-        mask: {
-            'position': 'fixed',
-            'top': 0,
-            'right': 0,
-            'bottom': 0,
-            'left': 0,
-            'background': 'rgba(50,50,50,.7)',
-            'z-index': 9999
-        },
-        wrap: {
-            'display': 'table',
-            'position': 'relative',
-            'min-width': '200px',
-            'max-width': '360px',
-            'margin': '200px auto 0',
-            'padding': '40px 30px',
-            'color': '#333',
-            'background': '#ffffff'
-        },
-        top: {
-            'display': 'none'
-        },
-        txt: {
-            'font-size': '16px',
-            'line-height': 1.5,
-            'padding': ' 0 0 20px 0',
-            'text-align': 'center',
-            'word-break': 'break-all',
-            'color': '#333'
-        },
-        foot: {
-            'text-align': 'center'
-        },
-        btn0: {
-            'display': 'inline-block',
-            'padding': '7px 25px',
-            'margin': '0 10px',
-            'color': '#fff',
-            'background': '#15b790',
-            'font-size': '14px',
-            'border': '1px solid #15b790',
-            'text-decoration': 'none'
-        },
-        btn1: {
-            'display': 'inline-block',
-            'padding': '7px 25px',
-            'margin': '0 10px',
-            'color': '#666',
-            'background': '#fff',
-            'font-size': '14px',
-            'border': '1px solid #eee',
-            'text-decoration': 'none'
-        },
-        x: {
-            'position': 'absolute',
-            'right': '10px',
-            'font-size': '22px',
-            'top': 0,
-            'color': '#999',
-            'text-decoration': 'none'
+    if (!document.getElementById(PLUGINNAME)) { //防止重复运行
+        //加入dialog最外层包裹
+        var domDialog = document.createElement('div');
+        domDialog.className = PLUGINNAME + '-dialog';
+        domDialog.innerHTML = '<div class="' + PLUGINNAME + '-mask">' +
+            '<div class="' + PLUGINNAME + '-wrap">' +
+            '<div class="' + PLUGINNAME + '-top"></div>' +
+            '<div class="' + PLUGINNAME + '-txt"></div>' +
+            '<div class="' + PLUGINNAME + '-foot">' +
+            '<a href="javascript:;" class="' + PLUGINNAME + '-btn0">确定</a>' +
+            '<a href="javascript:;" class="' + PLUGINNAME + '-btn1">取消</a>' +
+            '</div>' +
+            '<a href="javascript:;" class="' + PLUGINNAME + '-x">×</a>' +
+            '</div></div>';
+        document.body.appendChild(domDialog);
+
+        //加入toast
+        var domTost = document.createElement('div');
+        domTost.className = PLUGINNAME + '-toast';
+        document.body.appendChild(domTost);
+
+        //加入loading
+        var domLoading = document.createElement('div');
+        domLoading.className = PLUGINNAME + '-loading';
+        document.body.appendChild(domLoading);
+
+        // 公用属性或方法
+        var isIE8 = navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0";
+
+        function prevDefault(e) {
+            var e = e || window.event;
+            (e.preventDefault) ? e.preventDefault(): e.returnValue = false;
+        };
+
+        function showDom(dom, display) {
+            var _dis = display || 'block';
+            dom.style.display !== _dis && (dom.style.display = _dis);
+        };
+
+        function hideDom(dom) {
+            dom.style.display !== 'none' && (dom.style.display = 'none');
+        };
+
+        //样式
+        var styleObj = {
+            dialog: {
+                display: 'none',
+                position: 'relative',
+                'z-index': 9999
+            },
+            mask: {
+                'position': 'fixed',
+                'top': 0,
+                'right': 0,
+                'bottom': 0,
+                'left': 0,
+                'background': 'rgba(50,50,50,.7)'
+            },
+            wrap: {
+                'display': 'table',
+                'position': 'relative',
+                'min-width': '200px',
+                'max-width': '360px',
+                'margin': '200px auto 0',
+                'padding': '40px 30px',
+                'color': '#333',
+                'background': '#fff'
+            },
+            top: {
+                'display': 'none'
+            },
+            txt: {
+                'font-size': '16px',
+                'line-height': 1.5,
+                'padding': ' 0 0 20px 0',
+                'text-align': 'center',
+                'word-break': 'break-all',
+                'color': '#333'
+            },
+            foot: {
+                'text-align': 'center'
+            },
+            btn0: {
+                'display': 'inline-block',
+                'padding': '7px 25px',
+                'margin': '0 10px',
+                'color': '#fff',
+                'background': '#15b790',
+                'font-size': '14px',
+                'border': '1px solid #15b790',
+                'text-decoration': 'none'
+            },
+            btn1: {
+                'display': 'inline-block',
+                'padding': '7px 25px',
+                'margin': '0 10px',
+                'color': '#666',
+                'background': '#fff',
+                'font-size': '14px',
+                'border': '1px solid #eee',
+                'text-decoration': 'none'
+            },
+            x: {
+                'position': 'absolute',
+                'right': '10px',
+                'font-size': '22px',
+                'top': 0,
+                'color': '#999',
+                'text-decoration': 'none'
+            },
+            toast: {
+                'display': 'none',
+                'position': 'fixed',
+                'margin': 'auto',
+                'top': '30%',
+                'right': 0,
+                'left': 0,
+                'background': '#585e68',
+                'color': '#fff',
+                'line-height': 1.5,
+                'padding': '15px',
+                'width': '255px',
+                'text-align': 'center'
+            },
+            loading: {
+                'display': 'none',
+                'position': 'fixed',
+                'margin': 'auto',
+                'top': '30%',
+                'right': 0,
+                'left': 0,
+                'background': '#585e68',
+                'color': '#fff',
+                'line-height': 1.5,
+                'padding': '15px',
+                'width': '255px',
+                'text-align': 'center'
+            }
+        };
+        var arrStyle = [],
+            tmpStyle;
+        for (var k in styleObj) {
+            tmpStyle = styleObj[k];
+            arrStyle.push('.' + PLUGINNAME + '-' + k + '{');
+            for (var sk in tmpStyle) {
+                arrStyle.push(sk + ':' + tmpStyle[sk] + ';');
+            }
+            arrStyle.push('}');
         }
-    };
-    var arrStyle = [],
-        tmpStyle;
-    for (var k in styleObj) {
-        tmpStyle = styleObj[k];
-        arrStyle.push('.' + PLUGINNAME + '-' + k + '{');
-        for (var sk in tmpStyle) {
-            arrStyle.push(sk + ':' + tmpStyle[sk] + ';');
-        }
-        arrStyle.push('}');
-    }
-    //ie8 mask
-    arrStyle.push('@media \0screen{.' + PLUGINNAME + '-mask{background:#333;filter:Alpha(opacity=70);}}');
-    var HTML = '<div class="' + PLUGINNAME + '-mask"><div class="' + PLUGINNAME + '-wrap"><div class="' + PLUGINNAME + '-top">弹窗标题</div><div class="' + PLUGINNAME + '-txt">弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内</div><div class="' + PLUGINNAME + '-foot"><a href="javascript:;" class="' + PLUGINNAME + '-btn0">确定</a><a href="javascript:;" class="' + PLUGINNAME + '-btn1">取消</a></div><a href="javascript:;" class="' + PLUGINNAME + '-x">×</a></div></div>';
-    if (!document.getElementById(PLUGINNAME)) { //防止重复添加
+        //兼容ie8的mask
+        isIE8 && arrStyle.push('@media \\0screen{.' + PLUGINNAME + '-mask{background:rgb(50,50,50);filter:Alpha(opacity=70);}}');
+        //将style打入html，为了兼容ie8，别嫌奇怪
         var domTmp = document.createElement('div');
-        //为了兼容ie8，别嫌奇怪
-        domTmp.innerHTML = 'x<style id="' + PLUGINNAME + 'Style">' + arrStyle.join('') + '</style>';
+        domTmp.innerHTML = 'x<style id="' + PLUGINNAME + '">' + arrStyle.join('') + '</style>';
         document.getElementsByTagName('head')[0].appendChild(domTmp.lastChild);
-        var container = document.createElement('div');
-        container.style.display = 'none';
-        container.id = container.className = PLUGINNAME;
-        container.innerHTML = HTML;
-        document.body.appendChild(container);
+        var _mask = domDialog.children[0],
+            _wrap = _mask.children[0],
+            _foot = _wrap.children[2];
+
+        //主对象
+        var ui = {
+            doms: {
+                dialog: domDialog,
+                mask: _mask,
+                wrap: _wrap,
+                top: _wrap.children[0],
+                txt: _wrap.children[1],
+                foot: _foot,
+                x: _wrap.children[3],
+                btn0: _foot.children[0],
+                btn1: _foot.children[1],
+                toast: domTost,
+                loading: domLoading
+            },
+            alert: function(txt, fn, tit) {
+                var _fn = typeof fn === "function " ? fn : function() {},
+                    _dom = ui.doms;
+                _dom.txt.innerHTML = txt || "";
+                _dom.top.innerHTML = tit || "提示";
+                hideDom(_dom.btn1);
+                _dom.btn0.onclick = function(e) {
+                    prevDefault(e)
+                    _fn();
+                    hideDom(_dom.dialog);
+                };
+                showDom(_dom.dialog);
+            },
+            confirm: function(txt, fn1, fn2, tit) {
+                var _fn1 = typeof fn1 === "function " ? fn1 : function() {},
+                    _fn2 = typeof fn2 === "function " ? fn2 : function() {},
+                    _dom = ui.doms;
+                _dom.txt.innerHTML = txt || "";
+                _dom.top.innerHTML = tit || "提示 ";
+                showDom(_dom.btn1, 'inline-block');
+                _dom.btn0.onclick = function(e) {
+                    prevDefault(e)
+                    _fn1();
+                    hideDom(_dom.dialog);
+                };
+                _dom.btn1.onclick = function(e) {
+                    prevDefault(e)
+                    _fn2();
+                    hideDom(_dom.dialog);
+                };
+                showDom(_dom.dialog);
+            },
+            showLoading: function(txt) {
+                var _dom = ui.doms.loading,
+                    _txt = txt || '加载中…';
+                _dom.innerHTML !== _txt && (_dom.innerHTML = _txt);
+                showDom(_dom);
+            },
+            hideLoading: function() {
+                hideDom(ui.doms.loading);
+            },
+            toast: function(txt, delay, speed) {
+                var _delay = delay || 2000,
+                    _max = _delay,
+                    _speed = speed || 40,
+                    toastId = PLUGINNAME + 'Toast',
+                    _dom = ui.doms.toast;
+                if (_dom.style.display === 'block') { //防止多个toast同时存在
+                    return;
+                }
+                showDom(_dom);
+                _dom.innerHTML = txt;
+                isIE8 ? (_dom.style.filter = 'alpha(opacity=00)') : (_dom.style.opacity = 0);
+                var fadeIn = true,
+                    ie8filter, //专门记录ie8的透明度
+                    a = setInterval(function() {
+                        _delay -= _speed;
+                        isIE8 && (ie8filter = +_dom.style.filter.match(/\d/g).join(''));
+                        if (_delay <= _speed * 10 || _delay >= _max - _speed * 10) {
+                            if (fadeIn) {
+                                if (isIE8) {
+                                    _dom.style.filter = 'alpha(opacity=' + (ie8filter + 10) + ')';
+                                } else {
+                                    _dom.style.opacity = +_dom.style.opacity + 0.1;
+                                }
+                                if (_dom.style.opacity == 1 || ie8filter == 100) {
+                                    fadeIn = false;
+                                }
+                            } else {
+                                if (isIE8) {
+                                    _dom.style.filter = 'alpha(opacity=' + (ie8filter - 10) + ')';
+                                } else {
+                                    _dom.style.opacity -= 0.1;
+                                }
+                                if (_dom.style.opacity == 0 || ie8filter == 0) {
+                                    clearInterval(a);
+                                    hideDom(_dom);
+                                }
+                            }
+                        }
+                    }, _speed);
+            }
+        };
+        ui.doms.x.onclick = function(e) {
+            prevDefault(e)
+            hideDom(ui.doms.dialog);
+        };
+        window.ui = ui;
     };
-    var _mask = container.children[0],
-        _wrap = _mask.children[0],
-        _foot = _wrap.children[2];
-    var prevDefault = function(e) {
-        var e = e || window.event;
-        (e.preventDefault) ? e.preventDefault(): e.returnValue = false;
-    };
-    var ui = {
-        doms: {
-            container: container,
-            mask: _mask,
-            wrap: _wrap,
-            top: _wrap.children[0],
-            txt: _wrap.children[1],
-            foot: _foot,
-            x: _wrap.children[3],
-            btn0: _foot.children[0],
-            btn1: _foot.children[1]
-        },
-        show: function() {
-            ui.doms.container.style.display = "block";
-        },
-        hide: function() {
-            ui.doms.container.style.display = "none";
-        },
-        alert: function(txt, callback, tit) {
-            var _callback = typeof callback === "function " ? callback : function() {},
-                _dom = ui.doms;
-            _dom.txt.innerHTML = txt || "";
-            _dom.top.innerHTML = tit || "提示";
-            _dom.btn1.style.display = "none";
-            _dom.btn0.onclick = function(e) {
-                prevDefault(e)
-                _callback();
-                ui.hide();
-            };
-            ui.show();
-        },
-        confirm: function(txt, fn1, fn2, tit) {
-            var _fn1 = typeof fn1 === "function " ? fn1 : function() {},
-                _fn2 = typeof fn2 === "function " ? fn2 : function() {},
-                _dom = ui.doms;
-            _dom.txt.innerHTML = txt || "";
-            _dom.top.innerHTML = tit || "提示 ";
-            _dom.btn0.onclick = function(e) {
-                prevDefault(e)
-                _fn1();
-                ui.hide();
-            };
-            _dom.btn1.onclick = function(e) {
-                prevDefault(e)
-                _fn2();
-                ui.hide();
-            };
-            ui.show();
-        }
-    };
-    ui.doms.x.onclick = function(e) {
-        prevDefault(e)
-        ui.hide();
-    };
-    window.ui = ui;
 }()
